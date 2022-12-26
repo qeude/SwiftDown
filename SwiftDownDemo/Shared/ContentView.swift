b5df198c872d4679a47e0da83053c7f1289f1042
Quentin Eude <quentin.eude@getaround.com>
2022-12-26 14:04:46 +0100

    📝 Add focus example in demo

--- SwiftDownDemo/Shared/ContentView.swift
+++ SwiftDownDemo/Shared/ContentView.swift
@@ -10,12 +10,18 @@ import SwiftDown
 
 struct ContentView: View {
   @State private var text: String = "# H1\n## H2\n### H3\n#### H4\n##### H5\n###### H6\n**bold** __bold__\n*italic* _italic_\n`inline code`\n```\ncode block\n```\n\n> block quote\n\n- list\n\n1. list\n\n[link](link)\n\nbody"
-  
+  @FocusState private var focusedField: FocusField?
+
   var body: some View {
     VStack {
       SwiftDownEditor(text: $text, onTextChange: { text in
         print("onTextChange")
-      }).frame(height: 400)
+      })
+      .focused($focusedField, equals: .field)
+      .frame(height: 400)
+      .onAppear {
+        self.focusedField = .field
+      }
       TextField("", text: $text).frame(height: 50)
       Button {
         self.text = ""
@@ -23,6 +29,12 @@ struct ContentView: View {
         Text("Clear")
       }
     }
+  }
+}
+
+extension ContentView {
+  enum FocusField: Hashable {
+    case field
   }
 }
 
