1786119233f2628530c60cbca99d3768814fdca0
Ross Brandon <ross.brandon@pm.me>
2024-01-24 12:37:33 -0600

    Clean up comments in keyboard toolbar actions

--- Sources/SwiftDown/MarkdownKeyboardToolbar.swift
+++ Sources/SwiftDown/MarkdownKeyboardToolbar.swift
@@ -63,8 +63,6 @@ extension SwiftDown {
     )
   }
 
-  /// Inserts the H1 `#` tag
-  /// Adds 1 trailing whitespace at the current cursor position
   /// Moves the cursor position after the inserted characters
   @objc private func h1Action() {
     let selectedStart = self.selectedStart
@@ -73,8 +71,6 @@ extension SwiftDown {
     self.highlighter?.applyStyles()
   }
 
-  /// Inserts the H2 `##` tag
-  /// Adds 1 trailing whitespace at the current cursor position
   /// Moves the cursor position after the inserted characters
   @objc private func h2Action() {
     let selectedStart = self.selectedStart
@@ -83,8 +79,6 @@ extension SwiftDown {
     self.highlighter?.applyStyles()
   }
 
-  /// Inserts the H3 `###` tag
-  /// Adds 1 trailing whitespace at the current cursor position
   /// Moves the cursor position after the inserted characters
   @objc private func h3Action() {
     let selectedStart = self.selectedStart
@@ -93,7 +87,6 @@ extension SwiftDown {
     self.highlighter?.applyStyles()
   }
 
-  /// Inserts the bold`** **` tag
   /// If text is selected, surrounds the selected text with the bold tags
   /// Moves the cursor to the end of the selected text, if applicable
   @objc private func boldAction() {
@@ -105,7 +98,6 @@ extension SwiftDown {
     self.highlighter?.applyStyles()
   }
 
-  /// Inserts the italic`* *` tag
   /// If text is selected, surrounds the selected text with the italic tags
   /// Moves the cursor to the end of the selected text, if applicable
   @objc private func italicizeAction() {
@@ -117,9 +109,7 @@ extension SwiftDown {
     self.highlighter?.applyStyles()
   }
 
-  /// Inserts the unordered list`-` tag
   /// Adds 1 leading line break
-  /// Adds 1 trailing whitespace at the current cursor position
   @objc private func unorderedListAction() {
     let selectedStart = self.selectedStart
     self.text.insert(contentsOf: "\n- ", at: self.text.index(self.text.startIndex, offsetBy: selectedStart))
@@ -127,9 +117,7 @@ extension SwiftDown {
     self.highlighter?.applyStyles()
   }
 
-  /// Inserts the ordered list`1.` tag
   /// Adds 1 leading line break
-  /// Adds 1 trailing whitespace at the current cursor position1
   @objc private func orderedListAction() {
     let selectedStart = self.selectedStart
     self.text.insert(contentsOf: "\n1. ", at: self.text.index(self.text.startIndex, offsetBy: selectedStart))
@@ -137,15 +125,12 @@ extension SwiftDown {
     self.highlighter?.applyStyles()
   }
 
-  /// Inserts the block quote `>` tag
-  /// Adds 1 trailing whitespace at the current cursor position
   @objc private func blockQuoteAction() {
     let selectedStart = self.selectedStart
     self.text.insert(contentsOf: "> ", at: self.text.index(self.text.startIndex, offsetBy: selectedStart))
     self.highlighter?.applyStyles()
   }
 
-  /// Inserts the link`[]()` tag
   /// If text is selected, it is checked if it contains a link
   ///   If a link is detected, the selected text is placed inside the braces
   ///   If a link is not detected, the selected text is placed inside the parenthesis
@@ -177,9 +162,7 @@ extension SwiftDown {
     self.highlighter?.applyStyles()
   }
 
-  /// Inserts the code block tag with line breaks
   /// If text is selected, moves the selected text inside the code block
-  /// Adds line breaks to create the block
   /// Moves the cursor into the code block at the end of the selected text, if applicable
   @objc private func codeBlockAction() {
     let selectedStart = self.selectedStart
